bd74865fcfb20aef662df10317e87228c287cc6c
Jentsch <thomas.jentsch@accenture.com>
2021-07-08 17:47:01 +0200

    added metrics implementation

--- src/main/java/com/twodigits/FruitResource.java
+++ src/main/java/com/twodigits/FruitResource.java
@@ -11,11 +11,22 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
 @Path("fruits")
 public class FruitResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Counted(
+        name = "fruits.get.counter", 
+        description = "How many calls for get have been registered.")
+    @Timed(
+        name = "fruits.get.timer",
+        description = "How long did the invocations of get take time.",
+        unit = MetricUnits.MILLISECONDS)
     public List<Fruit> get() {
         return Fruit.listAll();
     }
@@ -24,6 +35,13 @@ public class FruitResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Counted(
+        name = "fruit.create.counter", 
+        description = "How many fruits  have been created.")
+    @Timed(
+        name = "fruit.create.timer",
+        description = "How long did the invocations of create take time.",
+        unit = MetricUnits.MILLISECONDS)
     public Response create(Fruit fruit) {
         fruit.id = null;
         fruit.persist();
